cec8a1ebde356d18f43ba7d8b3feafa122ae28b4
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2001-12-28 10:57:58 +0000

    * removed descendants of Item, Record and Poll, replaced by int code and constants * added field closed to Poll, which indicates, whether it is possible to vote in this poll * added method synchronizeWith to GenericObject and removed duplicit functionality from MySqlPersistance * completely changed tree handling. Added Relation, which is derived from GenericObject.   Now tree is handled standard way. * redefined meaning and fixed findParents
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@120 be82d567-0701-4079-a524-1ea639e27e29

---
+++ src/cz/abclinuxu/data/Relation.java
@@ -0,0 +1,88 @@
+/*
+ * User: literakl
+ * Date: Dec 26, 2001
+ * Copyright by Leos Literak (literakl@centrum.cz) 2001
+ */
+package cz.abclinuxu.data;
+
+/**
+ * This class contains one relation between two GenericObjects.
+ */
+public final class Relation extends GenericObject {
+
+    /** Upper relation. Similar to .. in filesystem. */
+    int upper = 0;
+    GenericObject parent;
+    GenericObject child;
+    /** Name of the relation. Used, when child's name is not descriptive (or unique) enough in parent's context. */
+    String name;
+
+
+    public Relation() {
+    }
+
+    public Relation(int id) {
+        super(id);
+    }
+
+    /**
+     * Constructs new Relation between <code>parent</code> and <code>child</code> (in this order).
+     * If this relation is not top-level (e.g. it has its parent), you shall set <code>upper</code>
+     * to <code>id</code> of this upper-level relation, otherwise set it to 0.
+     */
+    public Relation(GenericObject parent, GenericObject child, int upper) {
+        this.parent = parent;
+        this.child = child;
+        this.upper = upper;
+    }
+
+    /**
+     * @return Name of the relation. Used, when child's name is not descriptive (or unique) enough in parent's context.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name of the relation. Used, when child's name is not descriptive (or unique) enough
+     * in parent's context. Imagine Category USB from Modems, when linked to Category USB devices.
+     * It is better to name this new relation as Modems.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GenericObject getChild() {
+        return child;
+    }
+
+    public void setChild(GenericObject child) {
+        this.child = child;
+    }
+
+    public GenericObject getParent() {
+        return parent;
+    }
+
+    public void setParent(GenericObject parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * @return Upper relation. Similar to .. in filesystem.
+     */
+    public int getUpper() {
+        return upper;
+    }
+
+    /**
+     * Sets upper relation. Similar to .. in filesystem.
+     */
+    public void setUpper(int upper) {
+        this.upper = upper;
+    }
+
+    public String toString() {
+        return "Relation " +id+",upper="+upper;
+    }
+}
