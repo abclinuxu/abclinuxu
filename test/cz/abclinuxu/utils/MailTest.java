a8a5db4dc1c157618db415759c447b6d1af2ab8d
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2002-01-13 16:32:52 +0000

    java mail test
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@146 be82d567-0701-4079-a524-1ea639e27e29

---
+++ test/cz/abclinuxu/utils/MailTest.java
@@ -0,0 +1,41 @@
+/*
+ * User: literakl
+ * Date: Jan 13, 2002
+ * Time: 5:08:07 PM
+ * (c)2001-2002 Tinnio
+ */
+package cz.abclinuxu.utils;
+
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.InternetAddress;
+import java.util.Properties;
+
+public class MailTest {
+
+    public static void main(String[] args) {
+
+        try {
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props,null);
+            session.setDebug(true);
+
+            MimeMessage message = new MimeMessage(session);
+            message.setSubject("javamail works! ");
+            message.setText("wow, it really works! Yabadabadoo");
+
+            Address to = new InternetAddress("root");
+            Address from = new InternetAddress("literakl@raketa");
+
+            message.setFrom(from);
+            message.setRecipient(Message.RecipientType.TO,to);
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect("localhost",null,null);
+            message.saveChanges();
+            transport.sendMessage(message,message.getAllRecipients());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
