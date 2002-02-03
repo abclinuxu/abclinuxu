2b200f573c345b979d41f8a51d69e702fa67d574
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2002-02-03 07:08:42 +0000

    fix program for \n to <br>\n conversion
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@194 be82d567-0701-4079-a524-1ea639e27e29

---
+++ src/cz/abclinuxu/transfer/FixRecords.java
@@ -0,0 +1,49 @@
+/*
+ * User: literakl
+ * Date: Feb 3, 2002
+ * Time: 7:09:42 AM
+ * (c)2001-2002 Tinnio
+ */
+package cz.abclinuxu.transfer;
+
+import java.sql.*;
+
+public class FixRecords {
+
+    static {
+        try { Class.forName("org.gjt.mm.mysql.Driver"); } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conAbc = DriverManager.getConnection("jdbc:mysql://localhost/abc?user=literakl&password=lkaretil&useUnicode=true&characterEncoding=ISO-8859-2");
+        Statement statement = conAbc.createStatement();
+        ResultSet set = statement.executeQuery("select * from zaznam");
+
+        while ( set.next() ) {
+            String data = new String(set.getBytes(3));
+            String result = fixLines(data);
+            if (data!=result) {
+                set.updateBytes(3,result.getBytes());
+                set.updateRow();
+            }
+        }
+    }
+
+    public static String fixLines(String str) {
+        if ( (str.indexOf("&lt;")!=-1)&&(str.indexOf("&gt;")!=-1) ) return str;
+        StringBuffer sb = new StringBuffer(str);
+        for ( int i=0; i<sb.length(); i++ ) {
+            char c = sb.charAt(i);
+            if ( c=='\r' ) {
+                sb.deleteCharAt(i);
+                i--;
+                continue;
+            }
+            if ( c=='\n' ) {
+                sb.insert(i,"&lt;BR&gt;");
+                i += 10;
+            }
+        }
+        return sb.toString();
+    }
+}
