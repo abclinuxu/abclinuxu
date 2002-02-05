359c1d0969edef7ab52ea1f54203bf90da3fb7ba
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2002-02-05 13:42:46 +0000

    mass mail tool updated
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@206 be82d567-0701-4079-a524-1ea639e27e29

--- bin/mail.sh
+++ bin/mail.sh
@@ -3,6 +3,8 @@
 export LC_CTYPE=cs_Cz
 BUILD=../build/WEB-INF
 TOMCAT=/home/literakl/tomcat/common/lib
-export CLASSPATH=$CLASSPATH:$BUILD/classes:$TOMCAT/activation.jar:$TOMCAT/mail.jar
+LIBS=../libs
+JARS=$LIBS/crimson.jar:$LIBS/dom4j.jar:$LIBS/jdbc2_0-stdext.jar:$LIBS/log4j.jar:$LIBS/servlet.jar:$LIBS/jaxp.jar:$LIBS/mm.mysql-2.0.7-bin.jar:$LIBS/velocity.jar
+export CLASSPATH=$CLASSPATH:$BUILD/classes:$TOMCAT/activation.jar:$TOMCAT/mail.jar:$JARS
 
-java -cp $CLASSPATH cz.abclinuxu.utils.MailTest
+java -cp $CLASSPATH cz.abclinuxu.utils.Mailer $1 $2
