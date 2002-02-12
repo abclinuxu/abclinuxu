4d1e1557b13055f9dff76aa3ce0d37c88ed1212f
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2002-02-12 07:52:28 +0000

    celkem = 1017 ms ,prumer = 0.41510203 ms.
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@226 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/utils/Measure.java
+++ src/cz/abclinuxu/utils/Measure.java
@@ -14,6 +14,7 @@ import cz.abclinuxu.servlets.utils.VelocityHelper;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import java.util.List;
+import java.io.*;
 
 /**
  * This class works as template for speed measurement.
@@ -26,21 +27,24 @@ public class Measure {
         Persistance persistance = PersistanceFactory.getPersistance();
         int i=0,j=0;
         long l = 0;
+        String line = null;
 
         // place initilizaton here
-        List list = persistance.findByCommand("select count(cislo) from zaznam where typ=1");
+        BufferedReader in = new BufferedReader(new FileReader("/home/literakl/test.txt"));
+        while ( (line=in.readLine())!=null );
 
         long start = System.currentTimeMillis();
-        for (i=0; i<300; i++) {
+        for (i=0; i<2450; i++) {
             //place your code to measure here
-            list = persistance.findByCommand("select count(cislo) from zaznam where typ=1");
+            in = new BufferedReader(new FileReader("/home/literakl/test.txt"));
+            while ( (line=in.readLine())!=null );
         }
         long end = System.currentTimeMillis();
 
         // place clean up here
 
         float avg = (end-start)/(float)i;
-        System.out.println("celkem = "+(end-start)+" ,prumer = "+avg);
+        System.out.println("celkem = "+(end-start)+" ms ,prumer = "+avg+ " ms.");
         org.apache.log4j.Category.getDefaultHierarchy().enableAll();
     }
 }
