36204f0568350e05a9f4e1fc0820a04949e9dbfb
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2002-05-21 17:36:26 +0000

    RSS support
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@265 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/scheduler/Attic/jobs/GenerateLinks.java
+++ src/cz/abclinuxu/scheduler/Attic/jobs/GenerateLinks.java
@@ -18,11 +18,15 @@ import java.util.*;
 
 import org.apache.regexp.RE;
 import org.apache.regexp.RESyntaxException;
+import org.apache.log4j.BasicConfigurator;
 
 public class GenerateLinks implements Task {
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(GenerateLinks.class);
-    static String fileName = "abc.dat";
+
+    static String fileName_trafika = "abc.dat";
     static String fileName_anneca = "abc2.dat";
+    static String fileName_rss = "abc.rss";
+
     static RE lineBreak;
 
     static {
@@ -33,27 +37,40 @@ public class GenerateLinks implements Task {
         }
     }
 
+    LinksGenerator[] generators;
+
+
+    public GenerateLinks() {
+        generators = new LinksGenerator[3];
+        generators[0] = new Trafika();
+        generators[1] = new Anneca();
+        generators[2] = new RSS();
+    }
+
     public void runJob() {
         try {
-            FileWriter writer = new FileWriter(fileName);
-            FileWriter writer2 = new FileWriter(fileName_anneca);
             Persistance persistance = PersistanceFactory.getPersistance();
             VelocityHelper helper = new VelocityHelper();
-
-            writer.write(Constants.isoFormat.format(new Date()));
-            writer.write('\n');
-
             Category actual = (Category) persistance.findById(new Category(Constants.CAT_ACTUAL_ARTICLES));
             helper.sync(actual.getContent());
             List list = helper.sortByDateDescending(actual.getContent());
+
+            for (int j = 0; j < generators.length; j++) {
+                generators[j].generateHeader();
+            }
+
+            String url,title,desc;
             for(int i=0; i<4 && i<list.size(); i++ ) {
                 Relation relation = (Relation) list.get(i);
                 Item item = (Item) relation.getChild();
 
-                writer.write("http://AbcLinuxu.cz/clanky/ViewRelation?relationId="+relation.getId());
-                writer2.write("http://AbcLinuxu.cz/clanky/ViewRelation?relationId="+relation.getId());
-                writer.write("|\\"+helper.getXPath(item,"data/name")+"\n");
-                writer2.write("|"+helper.getXPath(item,"data/name")+"|"+removeNewLines(helper.getXPath(item,"data/perex"))+"\n");
+                url = "http://AbcLinuxu.cz/clanky/ViewRelation?relationId="+relation.getId();
+                title = helper.getXPath(item,"data/name");
+                desc = removeNewLines(helper.getXPath(item,"data/perex"));
+
+                for (int j = 0; j < generators.length; j++) {
+                    generators[j].generateLink(title,desc,url);
+                }
             }
 
             list = persistance.findByCommand("select cislo from zaznam where typ=1 order by kdy desc limit 2");
@@ -61,13 +78,15 @@ public class GenerateLinks implements Task {
                 Object[] objects = (Object[]) iter.next();
                 Relation child = new Relation(null,new Record(((Integer)objects[0]).intValue(),Record.HARDWARE),0);
                 Relation found = persistance.findByExample(child)[0];
-
                 Item item = (Item) found.getParent();
                 persistance.synchronize(item);
-                writer.write("http://AbcLinuxu.cz/hardware/ViewRelation?relationId="+found.getId());
-                writer2.write("http://AbcLinuxu.cz/hardware/ViewRelation?relationId="+found.getId());
-                writer.write("|\\H "+helper.getXPath(item,"data/name")+"\n");
-                writer2.write("|H "+helper.getXPath(item,"data/name")+"|\n");
+
+                url = "http://AbcLinuxu.cz/hardware/ViewRelation?relationId="+found.getId();
+                title = "H "+helper.getXPath(item,"data/name");
+
+                for (int j = 0; j < generators.length; j++) {
+                    generators[j].generateLink(title,"",url);
+                }
             }
 
             list = persistance.findByCommand("select cislo from zaznam where typ=2 order by kdy desc limit 2");
@@ -75,13 +94,15 @@ public class GenerateLinks implements Task {
                 Object[] objects = (Object[]) iter.next();
                 Relation child = new Relation(null,new Record(((Integer)objects[0]).intValue(),Record.SOFTWARE),0);
                 Relation found = persistance.findByExample(child)[0];
-
                 Item item = (Item) found.getParent();
                 persistance.synchronize(item);
-                writer.write("http://AbcLinuxu.cz/software/ViewRelation?relationId="+found.getId());
-                writer2.write("http://AbcLinuxu.cz/software/ViewRelation?relationId="+found.getId());
-                writer.write("|\\S "+helper.getXPath(item,"data/name")+"\n");
-                writer2.write("|S "+helper.getXPath(item,"data/name")+"|\n");
+
+                url = "http://AbcLinuxu.cz/software/ViewRelation?relationId="+found.getId();
+                title = "S "+helper.getXPath(item,"data/name");
+
+                for (int j = 0; j < generators.length; j++) {
+                    generators[j].generateLink(title,"",url);
+                }
             }
 
             list = persistance.findByCommand("select cislo from polozka where typ=5 order by kdy desc limit 2");
@@ -89,17 +110,20 @@ public class GenerateLinks implements Task {
                 Object[] objects = (Object[]) iter.next();
                 Relation child = new Relation(null,new Item(((Integer)objects[0]).intValue(),Item.DRIVER),0);
                 Relation found = persistance.findByExample(child)[0];
-
                 Item item = (Item) found.getChild();
                 persistance.synchronize(item);
-                writer.write("http://AbcLinuxu.cz/drivers/ViewRelation?relationId="+found.getId());
-                writer2.write("http://AbcLinuxu.cz/drivers/ViewRelation?relationId="+found.getId());
-                writer.write("|\\O "+helper.getXPath(item,"data/name")+"\n");
-                writer2.write("|O "+helper.getXPath(item,"data/name")+"|\n");
+
+                url = "http://AbcLinuxu.cz/drivers/ViewRelation?relationId="+found.getId();
+                title = "O "+helper.getXPath(item,"data/name");
+
+                for (int j = 0; j < generators.length; j++) {
+                    generators[j].generateLink(title,"",url);
+                }
             }
 
-            writer.close();
-            writer2.close();
+            for (int j = 0; j < generators.length; j++) {
+                generators[j].generateBottom();
+            }
         } catch (Exception e) {
             log.error("Cannot generate links",e);
         }
@@ -113,7 +137,7 @@ public class GenerateLinks implements Task {
      * Sets default file name.
      */
     public static void setFileName(String name) {
-        fileName = name;
+        fileName_trafika = name;
     }
 
     /**
@@ -131,7 +155,97 @@ public class GenerateLinks implements Task {
     }
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         GenerateLinks links = new GenerateLinks();
         links.runJob();
+    }
+
+    /**
+     * For event based generation of files with links.
+     */
+    interface LinksGenerator {
+        /** called at start */
+        public void generateHeader() throws IOException;
+        /** called at end */
+        public void generateBottom() throws IOException;
+        /** called for each article */
+        public void generateLink(String title, String desc, String url) throws IOException;
+    }
+
+    /**
+     * classical trafika format
+     */
+    class Trafika implements LinksGenerator {
+        FileWriter writer = null;
+
+        public void generateHeader() throws IOException {
+            writer = new FileWriter(fileName_trafika);
+            writer.write(Constants.isoFormat.format(new Date()));
+            writer.write('\n');
+        }
+
+        public void generateBottom() throws IOException {
+            writer.close();
+        }
+
+        public void generateLink(String title, String desc, String url) throws IOException {
+            writer.write(url+"|\\"+title+"\n");
+        }
+    }
+
+    /**
+     * Slightly modified and enhanced Trafika format
+     */
+    class Anneca implements LinksGenerator {
+        FileWriter writer = null;
+
+        public void generateHeader() throws IOException {
+            writer = new FileWriter(fileName_anneca);
+        }
+
+        public void generateLink(String title, String desc, String url) throws IOException {
+            writer.write(url+"|"+title+"|"+desc+"\n");
+        }
+
+        public void generateBottom() throws IOException {
+            writer.close();
+        }
+    }
+
+    /**
+     * Standard RSS
+     */
+    class RSS implements LinksGenerator {
+        FileWriter writer = null;
+
+        public void generateHeader() throws IOException {
+            writer = new FileWriter(fileName_rss);
+            writer.write("<?xml version=\"1.0\" encoding=\"iso-8859-2\" ?>\n");
+            writer.write("<!DOCTYPE rss PUBLIC \"-//Netscape Communications//DTD RSS 0.91//EN\" \"http://my.netscape.com/publish/formats/rss-0.91.dtd\">\n");
+            writer.write("<rss version=\"0.91\">\n");
+            writer.write("<channel>\n");
+            writer.write("<title>AbcLinuxu.cz - tady je tuèòákùm hej!</title>\n");
+            writer.write("<link>http://AbcLinuxu.cz</link>\n");
+            writer.write("<description>Centrála pro výmìnu rad, zku¹eností a návodù pod Linuxem.</description>\n");
+        }
+
+        public void generateLink(String title, String desc, String url) throws IOException {
+            writer.write("<item>\n");
+            writer.write("<title>"+title+"</title>\n");
+            writer.write("<link>"+url+"</link>\n");
+            writer.write("</item>\n");
+        }
+
+        public void generateBottom() throws IOException {
+            writer.write("<textinput>\n");
+            writer.write("<title>Prohledej AbcLinuxu.cz</title>\n");
+            writer.write("<description>Hledej výraz v návodech, èláncích èi diskusích.</description>\n");
+            writer.write("<name>query</name>\n");
+            writer.write("<link>http://AbcLinuxu.cz/Search</link>\n");
+            writer.write("</textinput>\n");
+            writer.write("</channel>\n");
+            writer.write("</rss>\n");
+            writer.close();
+        }
     }
 }
