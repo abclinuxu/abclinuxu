feae044b0275947f5ebc956f5a391f5e55b6281d
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-04-03 13:19:04 +0000

    agresivnejsi uvolnovani pameti, logovani informaci
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@324 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/persistance/DefaultCache.java
+++ src/cz/abclinuxu/persistance/DefaultCache.java
@@ -24,18 +24,19 @@ public class DefaultCache extends TimerTask implements Cache {
     long modCount;
 
     /**
-     * Every cached object will be deleted, if it was not synchronized with last hour.
+     * Every cached object will be deleted, if it was not synchronized with last 15 minutes.
      * This way we propagate external changes from database to model and
      * reduce consequences of possible errors of Cache.
      */
     public static final int SYNC_INTERVAL = 15*60*1000; // 15 minutes
     /** cached objects, which were not accessed within this interval, will be deleted */
-    public static final int MAX_LRU = 15*60*1000; // 15 minutes
+    public static final int MAX_LRU = 5*60*1000; // 5 minutes
 
     public DefaultCache() {
         data = new HashMap(100);
         modCount = 0;
-        AbcInit.getScheduler().schedule(this,SYNC_INTERVAL,3*60*1000);
+        AbcInit.getScheduler().schedule(this,((int)1.5*MAX_LRU),MAX_LRU);
+//        AbcInit.getScheduler().schedule(this,((int)0.5*MAX_LRU),3*60*1000);
     }
 
     /**
@@ -154,12 +155,13 @@ public class DefaultCache extends TimerTask implements Cache {
      * will scan all CachedObjects, and LRU objects are removed from cache.
      */
     public void run() {
+        Runtime runtime = Runtime.getRuntime();
+        log.info("cache: "+data.size()+" objects, memory: "+runtime.freeMemory()+"/"+runtime.totalMemory());
         long expectedModCount = modCount;
+        long now = System.currentTimeMillis(); // calculated only once to make it as fast as possible
 
         try {
             Iterator iter = data.values().iterator();
-
-            long now = System.currentTimeMillis(); // calculated only once to make it as fast as possible
             while ( expectedModCount==modCount && iter.hasNext() ) {
                 CachedObject cached = (CachedObject) iter.next();
 
@@ -169,6 +171,8 @@ public class DefaultCache extends TimerTask implements Cache {
                     iter.remove();
                 }
             }
+            runtime.gc();
+            log.info("cache purged: "+data.size()+" objects, memory: "+runtime.freeMemory()+"/"+runtime.totalMemory());
         } catch (Exception e) {
             log.warn("Cache synchronization threw an exception!",e);
         }
