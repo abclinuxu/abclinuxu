b61917f9aa731252beceb45489824938ca042b92
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-08-03 20:45:42 +0000

    new configuration uses filters - finaly I can handle deploy on more machines
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@404 be82d567-0701-4079-a524-1ea639e27e29

--- bin/mailer.sh
+++ bin/mailer.sh
@@ -3,10 +3,9 @@
 #export LC_CTYPE=cs_Cz
 export LC_CTYPE=czech
 
-LIBS=/home/literakl/abc/source/libs
-DEPLOY=/home/literakl/abc/deploy
-BUILD=$DEPLOY/WEB-INF/classes
-ARGS="-Dlog4j.configuration=file:$DEPLOY/WEB-INF/conf/log4j.xml -Dabc.config=$DEPLOY/WEB-INF/conf/systemPrefs.xml"
+LIBS=@SOURCE_DIR@/libs
+BUILD=@DEPLOY_ABCLINUXU@/WEB-INF/classes
+ARGS="-Dlog4j.configuration=file:@DEPLOY_ABCLINUXU@/WEB-INF/conf/log4j.xml -Dabc.config=@DEPLOY_ABCLINUXU@/WEB-INF/conf/systemPrefs.xml"
 
 JARS=$LIBS/dom4j.jar:$LIBS/log4j.jar:$LIBS/mysql-connector.jar:$LIBS/proxool.jar
 JARS=$JARS:$LIBS/servlet.jar:$LIBS/freemarker.jar
--- bin/onetime/migrateUser.sh
+++ bin/onetime/migrateUser.sh
@@ -3,10 +3,12 @@
 export LC_CTYPE=czech
 
 LIBS=/home/literakl/abc/source/libs
-BUILD=/home/literakl/abc/deploy/WEB-INF/classes
+DEPLOY=/home/www-data/deploy/abclinuxu
+ARGS="-Dlog4j.configuration=file:$DEPLOY/WEB-INF/conf/log4j.xml -Dabc.config=$DEPLOY/WEB-INF/conf/systemPrefs.xml"
+BUILD=$DEPLOY/WEB-INF/classes
 
 JARS=$LIBS/dom4j.jar:$LIBS/log4j.jar
 JARS=$JARS:$LIBS/mysql-connector.jar:$LIBS/proxool.jar
 export CLASSPATH=$CLASSPATH:$BUILD:$JARS
 
-java -cp $CLASSPATH cz.abclinuxu.migrate.UpgradeUser
+java $ARGS -cp $CLASSPATH cz.abclinuxu.migrate.UpgradeUser
