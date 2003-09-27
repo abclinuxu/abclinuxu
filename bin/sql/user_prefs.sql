87e3ef3a2b863073f5adcdc07fbe6fec4aacf4c2
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-09-27 14:30:15 +0000

    caste prikazy tykajici se uzivatelu
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@457 be82d567-0701-4079-a524-1ea639e27e29

---
+++ bin/sql/user_prefs.sql
@@ -0,0 +1,9 @@
+select count(*) from uzivatel where data like '%<weekly_summary>yes%';
+select count(*) from uzivatel where data like '%<newsletter>yes%';
+select count(*) from uzivatel where data like '%<emoticons>yes%';
+select count(*) from uzivatel where data like '%<emoticons>no%';
+select count(*) from uzivatel where data like '%<last_login_date>%';
+
+select email from uzivatel where data like '%<email valid="yes">%<newsletter>yes%';
+
+
