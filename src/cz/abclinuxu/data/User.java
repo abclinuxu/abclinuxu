b33b3c67571e3978e20ee1f308ba09998b8d7779
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-11-02 17:04:38 +0000

    cache pro role
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@474 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/data/User.java
+++ src/cz/abclinuxu/data/User.java
@@ -6,8 +6,7 @@ package cz.abclinuxu.data;
 import org.dom4j.Document;
 import org.dom4j.Node;
 
-import java.util.List;
-import java.util.Iterator;
+import java.util.*;
 
 import cz.abclinuxu.security.Roles;
 
@@ -25,6 +24,8 @@ public class User extends GenericObject implements XMLContainer {
     private String password;
     /** XML with data or this object */
     private XMLHandler documentHandler;
+    /** cache of granted user roles */
+    private Map roles = null;
 
 
     public User() {
@@ -118,6 +119,7 @@ public class User extends GenericObject implements XMLContainer {
      * sets XML data of this object
      */
     public void setData(Document data) {
+        roles = null;
         documentHandler = new XMLHandler(data);
     }
 
@@ -125,8 +127,8 @@ public class User extends GenericObject implements XMLContainer {
      * sets XML data of this object in String format
      */
     public void setData(String data) {
-        documentHandler = new XMLHandler();
-        documentHandler.setData(data);
+        roles = null;
+        documentHandler = new XMLHandler(data);
     }
 
     /**
@@ -145,20 +147,31 @@ public class User extends GenericObject implements XMLContainer {
     public boolean hasRole(String role) {
         if ( id==1 ) return true;
         if ( role==null || role.length()==0 ) return false;
-        Document data = getData();
-        if ( data==null ) return false;
 
-        List nodes = data.selectNodes("/data/roles/role");
-        if (nodes==null || nodes.size()==0)
-            return false;
+        if (roles==null) {
+            Document data = getData();
+            if ( data==null ) {
+                roles = Collections.EMPTY_MAP;
+                return false;
+            }
 
-        for ( Iterator iter = nodes.iterator(); iter.hasNext(); ) {
-            String value = ((Node) iter.next()).getText();
-            if ( Roles.ROOT.equals(value))
-                return true;
-            if ( role.equals(value) )
-                return true;
+            List nodes = data.selectNodes("/data/roles/role");
+            if ( nodes==null || nodes.size()==0 ) {
+                roles = Collections.EMPTY_MAP;
+                return false;
+            }
+
+            roles = new HashMap(nodes.size(),1.0f);
+            for ( Iterator iter = nodes.iterator(); iter.hasNext(); ) {
+                String value = ((Node) iter.next()).getText();
+                roles.put(value,value);
+            }
         }
+
+        if ( roles.containsKey(Roles.ROOT) )
+            return true;
+        if ( roles.containsKey(role) )
+            return true;
         return false;
     }
 
