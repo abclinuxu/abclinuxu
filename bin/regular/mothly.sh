df2aceaaa41a8877cc56542ce39cd93a0340fc8b
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-11-05 10:05:01 +0000

    user can choose its own file, where to dump data
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@485 be82d567-0701-4079-a524-1ea639e27e29

--- bin/regular/mothly.sh
+++ bin/regular/mothly.sh
@@ -1,1 +1,1 @@
-mysql abc -e "select cislo from uzivatel where data like '%<email valid=\"yes\">%<newsletter>yes%'" >users.txt
+mysql abc -e "select cislo from uzivatel where data like '%<email valid=\"yes\">%<newsletter>yes%'"
