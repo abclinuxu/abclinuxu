db21801ee1084dfc4aeb3195e3a057f01175d913
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2003-11-06 07:20:11 +0000

    lepsi jmeno
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@490 be82d567-0701-4079-a524-1ea639e27e29

--- synchronize.sh
+++ synchronize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . -type f -and -newer checkpoint -not -path '*CVS*' | tar czf ~/source.tgz -T -
+find . -type f -and -newer checkpoint -not -path '*CVS*' | tar czf ~/zmeny.tgz -T -
 mv checkpoint orig.checkpoint
 touch checkpoint
