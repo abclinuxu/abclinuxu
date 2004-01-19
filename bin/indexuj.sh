353b6c713c61cc3c47a0e46448c75d9f2f216053
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2004-01-19 21:34:55 +0000

    podpora pro indexaci dat bez preruseni funkcnosti vyhledavani
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@569 be82d567-0701-4079-a524-1ea639e27e29

--- bin/indexuj.sh
+++ bin/indexuj.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LC_CTYPE=cs_Cz
+#export JAVA_HOME=/usr/local/java
 
 LIBS=@SOURCE_DIR@/libs
 BUILD=@DEPLOY_ABCLINUXU@/WEB-INF/classes
@@ -8,8 +9,23 @@ ARGS="-Xmx200m -Dlog4j.configuration=file:@DEPLOY_ABCLINUXU@/WEB-INF/conf/log4j.
 
 JARS=$LIBS/dom4j.jar:$LIBS/log4j.jar:$LIBS/mysql-connector.jar:$LIBS/proxool.jar
 JARS=$JARS:$LIBS/lucene.jar:$LIBS/regexp.jar:$LIBS/servlet.jar
-JARS=$JARS:$LIBS/activation.jar:$LIBS/mail.jar:$LIBS/velocity.jar:$LIBS/commons-collections.jar
+JARS=$JARS:$LIBS/activation.jar:$LIBS/mail.jar:$LIBS/commons-collections.jar
 export CLASSPATH=$CLASSPATH:$BUILD:$JARS
 
-java $ARGS -cp $CLASSPATH cz.abclinuxu.utils.search.CreateIndex $1 $2 $3 $4 $5
+INDEX_REAL=@DEPLOY_ABCLINUXU@/WEB-INF/index
+INDEX_TMP=@DEPLOY_ABCLINUXU@/WEB-INF/index_tmp
 
+#create temporary directory for index
+mkdir -p ${INDEX_TMP}
+
+#index data in this directory
+java $ARGS -cp $CLASSPATH cz.abclinuxu.utils.search.CreateIndex ${INDEX_TMP}
+
+#remove old index
+rm ${INDEX_REAL}/*
+
+#move new index to correct location
+mv ${INDEX_TMP}/* ${INDEX_REAL}
+
+#remove temporary directory
+rmdir ${INDEX_TMP}
