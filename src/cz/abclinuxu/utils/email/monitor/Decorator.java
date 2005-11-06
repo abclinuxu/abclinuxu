db4308a062ce36616a04646893316d4dfb2583fe
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2005-11-06 20:26:45 +0000

    bugfix #332 - zmenena licence na GPL
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@1417 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/utils/email/monitor/Decorator.java
+++ src/cz/abclinuxu/utils/email/monitor/Decorator.java
@@ -1,12 +1,24 @@
+/*
+ *  Copyright (C) 2005 Leos Literak
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ */
 package cz.abclinuxu.utils.email.monitor;
 
 import java.util.Map;
-
-/*
- * User: literakl
- * Date: 6.11.2003
- * Time: 12:13:05
- */
 
 /**
  * Serializes object. It works as decorator - based on
