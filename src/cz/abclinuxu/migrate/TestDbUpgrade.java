efac2daee88893882abb1c10425d906d5cafb9f4
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2007-06-05 19:25:05 +0000

    utility class to test whether db upgrade will break data
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@1962 be82d567-0701-4079-a524-1ea639e27e29

---
+++ src/cz/abclinuxu/migrate/TestDbUpgrade.java
@@ -0,0 +1,34 @@
+package cz.abclinuxu.migrate;
+
+import cz.abclinuxu.persistence.Persistence;
+import cz.abclinuxu.persistence.PersistenceFactory;
+import cz.abclinuxu.data.Category;
+import org.dom4j.Element;
+
+/**
+ * Utility class that updates selected category row in database with
+ * text containing diacritics. This way we can find out whether system locale
+ * is correctly set and data model upgrade will not harm data.
+ */
+public class TestDbUpgrade {
+    private static final String TEST_CHARS = "ěščřžýáí";
+    private static final int TEST_CATEGORY = 507;
+
+    public static void main(String[] args) {
+        Persistence persistence = PersistenceFactory.getPersistance();
+        Category category = (Category) persistence.findById(new Category(TEST_CATEGORY));
+        Element element = (Element) category.getData().selectSingleNode("/data/note");
+        String content;
+        if (element == null) {
+            element = category.getData().getRootElement().addElement("note");
+            content = "";
+        } else
+            content = element.getText();
+        content = content.concat(TEST_CHARS);
+        element.setText(content);
+        category.setSubType(TEST_CHARS);
+        persistence.update(category);
+        System.out.println("Text set to: '" + content + "', subtype to " + TEST_CHARS + ".");
+        System.out.println("Verify with 'select * from kategorie where cislo=" + TEST_CATEGORY+ "'");
+    }
+}
