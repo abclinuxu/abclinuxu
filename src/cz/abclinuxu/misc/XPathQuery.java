26c9e7d7990c60a1d5d2fe5224f9dfa1753ae868
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2007-07-17 19:06:17 +0000

    pokus zajistit spravne parametry
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@2028 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/misc/XPathQuery.java
+++ src/cz/abclinuxu/misc/XPathQuery.java
@@ -41,17 +41,18 @@ import org.dom4j.Node;
 public class XPathQuery {
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
+        System.out.println("Input parameters:");
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            System.out.println("arg " + i + " = " + arg);
+        }
+
+        if (args.length < 2) {
             System.out.println("Usage: XPathQuery \"sql query\" \"xpath query\"");
-            System.out.println("Sql query must return column with well formed XML in first column,");
+            System.out.println("Sql query must return column with well formed XML in the first column,");
             System.out.println("additional columns in SQL result will be dumped to output.");
-            System.out.println("xpath query contains your query, matching nodes will be printed.");
+            System.out.println("Xpath query contains your query, only matching nodes will be printed.");
             System.out.println();
-            System.out.println("Your parameters:");
-            for (int i = 0; i < args.length; i++) {
-                String arg = args[i];
-                System.out.println("arg " + i + " = " + arg);
-            }
             System.exit(1);
         }
 
