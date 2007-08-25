5305226667397dc6c8d79be5a6853b9d2d4b651c
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2007-08-25 18:34:35 +0000

    pridana moznost zaznamenavat delku vyplnovani ankety
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@2061 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/servlets/html/view/ShowSurvey.java
+++ src/cz/abclinuxu/servlets/html/view/ShowSurvey.java
@@ -59,31 +59,35 @@ public class ShowSurvey implements AbcAction {
     public static final String PARAM_SURVEY_ID = "surveyId";
 
     public static final String ATTRIB_DATA = "survey";
+    public static final String ATTRIB_STARTED = "started";
 
-    /** screen with this id starts survey */
+    /**
+     * screen with this id starts survey
+     */
     public static final String START_ID = "START";
 
     /* because we dont have always relation for survey, we have to simulate it with high enough number,
-     * so collision with existing relation is not possible
-     */
+    * so collision with existing relation is not possible
+    */
     public static final int SURVEY_PREFIX = 30000000;
 
     public String process(HttpServletRequest request, HttpServletResponse response, Map env) throws Exception {
         Persistence persistence = PersistenceFactory.getPersistence();
         Map params = (Map) env.get(Constants.VAR_PARAMS);
         User user = (User) env.get(Constants.VAR_USER);
+        HttpSession session = request.getSession();
 
         Item survey = (Item) InstanceUtils.instantiateParam(PARAM_SURVEY_ID, Item.class, params, request);
-        if ( survey == null ) {
-            ServletUtils.addError(Constants.ERROR_GENERIC, "Anketa nebyla nalezena!", env, request.getSession());
+        if (survey == null) {
+            ServletUtils.addError(Constants.ERROR_GENERIC, "Anketa nebyla nalezena!", env, session);
             UrlUtils urlUtils = (UrlUtils) env.get(Constants.VAR_URL_UTILS);
             urlUtils.redirect(response, "/");
             return null;
         }
 
         persistence.synchronize(survey);
-        if ( survey.getType() != Item.SURVEY ) {
-            ServletUtils.addError(Constants.ERROR_GENERIC, "Tento objekt není anketa!", env, request.getSession());
+        if (survey.getType() != Item.SURVEY) {
+            ServletUtils.addError(Constants.ERROR_GENERIC, "Tento objekt není anketa!", env, session);
             UrlUtils urlUtils = (UrlUtils) env.get(Constants.VAR_URL_UTILS);
             urlUtils.redirect(response, "/");
             return null;
@@ -91,60 +95,61 @@ public class ShowSurvey implements AbcAction {
 
         String currentScreen = (String) params.get(PARAM_SCREEN_CURRENT);
         String nextScreen = (String) params.get(PARAM_SCREEN_NEXT);
-        if ( Misc.empty(nextScreen) )
+        if (Misc.empty(nextScreen))
             nextScreen = START_ID;
 
-        String xpath = "//screen[@id='"+nextScreen+"']";
+        String xpath = "//screen[@id='" + nextScreen + "']";
         Element screen = (Element) survey.getData().selectSingleNode(xpath);
 
-        if ( screen == null ) {
-            log.error("Survey "+survey.getId()+" does not define screen "+nextScreen+" (called from "+currentScreen+")!");
-            ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, request.getSession());
+        if (screen == null) {
+            log.error("Survey " + survey.getId() + " does not define screen " + nextScreen + " (called from " + currentScreen + ")!");
+            ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, session);
             UrlUtils urlUtils = (UrlUtils) env.get(Constants.VAR_URL_UTILS);
             urlUtils.redirect(response, "/");
             return null;
         }
 
-        boolean mustBeLogged = screen.attributeValue("onlyUsers") != null;
-        boolean forbidDuplicates = screen.attributeValue("check") != null;
+        boolean mustBeLogged = Boolean.valueOf(screen.attributeValue("onlyUsers"));
+        boolean forbidDuplicates = Boolean.valueOf(screen.attributeValue("check"));
+        boolean recordTime = screen.attributeValue("recordTime") != null;
 
         if (mustBeLogged && user == null)
-                return FMTemplateSelector.select("ViewUser", "login", env, request);
+            return FMTemplateSelector.select("ViewUser", "login", env, request);
 
-        if (! Misc.empty(currentScreen))
-            saveParameters(request.getSession(), params, currentScreen);
+        if (!Misc.empty(currentScreen))
+            saveParameters(session, params, currentScreen);
+        if (recordTime)
+            session.setAttribute(ATTRIB_STARTED, new Date());
 
         if (forbidDuplicates)
             try {
                 Relation relation = new Relation(survey, survey, 0);
-                relation.setId(survey.getId()+SURVEY_PREFIX);
+                relation.setId(survey.getId() + SURVEY_PREFIX);
                 AccessKeeper.checkAccess(relation, user, "survey", request, response);
             } catch (AccessDeniedException e) {
-                if ( e.isIpAddressBlocked() && ! mustBeLogged)
+                if (e.isIpAddressBlocked() && !mustBeLogged)
                     return ServletUtils.showErrorPage("Z této IP adresy se už volilo. Zkuste to později.", env, request);
                 else
                     return ServletUtils.showErrorPage("Už jste jednou volil!", env, request);
             }
 
         Element dump = screen.element("dump");
-        if ( dump != null ) {
-            Document data = (Document) request.getSession().getAttribute(ATTRIB_DATA);
+        if (dump != null) {
             try {
-                dump(data, dump);
+                dump(session, dump);
             } catch (Exception e) {
-                log.error("Error in survey "+survey.getId(), e);
-                ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, request.getSession());
+                log.error("Error in survey " + survey.getId(), e);
+                ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, session);
                 UrlUtils urlUtils = (UrlUtils) env.get(Constants.VAR_URL_UTILS);
                 urlUtils.redirect(response, "/");
                 return null;
             }
-            request.getSession().removeAttribute(ATTRIB_DATA);
         }
 
         Element template = screen.element("template");
-        if ( template==null || template.getTextTrim().length()==0 ) {
-            log.error("Survey "+survey.getId()+" does not define template in screen "+nextScreen+"!");
-            ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, request.getSession());
+        if (template == null || template.getTextTrim().length() == 0) {
+            log.error("Survey " + survey.getId() + " does not define template in screen " + nextScreen + "!");
+            ServletUtils.addError(Constants.ERROR_GENERIC, "Omlouváme se, ale v anketě nastala chyba.", env, session);
             UrlUtils urlUtils = (UrlUtils) env.get(Constants.VAR_URL_UTILS);
             urlUtils.redirect(response, "/");
         }
@@ -154,40 +159,41 @@ public class ShowSurvey implements AbcAction {
     }
 
     /**
-     * this method ads all parameters, that shall be saved, to Document data bound to
+     * This method adds all parameters, that shall be saved, to Document data bound to
      * atrribute in session.
      */
     void saveParameters(HttpSession session, Map params, String currentScreen) {
         Document data = (Document) session.getAttribute(ATTRIB_DATA);
-        if ( data==null ) {
+        if (data == null) {
             data = DocumentHelper.createDocument();
             data.addElement("anketa");
-            session.setAttribute(ATTRIB_DATA,data);
+            session.setAttribute(ATTRIB_DATA, data);
         }
         Element screen = data.getRootElement().addElement("screen");
-        screen.addAttribute("id",currentScreen);
+        screen.addAttribute("id", currentScreen);
 
         String saveParams = (String) params.get(PARAM_SAVE_PARAMS);
-        if ( saveParams==null ) saveParams = "";
-        StringTokenizer stk = new StringTokenizer(saveParams,", \r\n");
+        if (saveParams == null)
+            saveParams = "";
+        StringTokenizer stk = new StringTokenizer(saveParams, ", \r\n");
 
-        while ( stk.hasMoreTokens() ) {
+        while (stk.hasMoreTokens()) {
             String var = stk.nextToken();
             var = var.trim();
             Object value = params.get(var);
-            if (value==null)
+            if (value == null)
                 continue;
-            if ( value instanceof String ) {
+            if (value instanceof String) {
                 String s = (String) value;
-                if ( !Misc.empty(s) )
+                if (!Misc.empty(s))
                     screen.addElement(var).addText(s);
             } else {
                 List list = (List) value;
-                if ( Misc.empty(list) )
+                if (Misc.empty(list))
                     continue;
                 for (Iterator iter = list.iterator(); iter.hasNext();) {
                     String s = (String) iter.next();
-                    if ( !Misc.empty(s) )
+                    if (!Misc.empty(s))
                         screen.addElement(var).addText(s);
                 }
             }
@@ -197,51 +203,62 @@ public class ShowSurvey implements AbcAction {
     /**
      * Dumps data to file specified in element dump.
      */
-    void dump(Document data, Element dump) throws Exception {
+    void dump(HttpSession session, Element dump) throws Exception {
+        Document data = (Document) session.getAttribute(ATTRIB_DATA);
+        session.removeAttribute(ATTRIB_DATA);
+        Date started = (Date) session.getAttribute(ATTRIB_STARTED);
+        if (started != null) {
+            long time = (System.currentTimeMillis() - started.getTime()) / 1000;
+            List screens = data.getRootElement().elements();
+            Element lastScreen = (Element) screens.get(screens.size() -1);
+            lastScreen.addElement("recordedTime").addText(Long.toString(time));
+        }
+
         StringWriter sWriter = new StringWriter();
         OutputFormat format = OutputFormat.createPrettyPrint();
         format.setEncoding("UTF-8");
         XMLWriter xmlWriter = new XMLWriter(sWriter, format);
         xmlWriter.write(data);
         xmlWriter.flush();
+        String xmlString = sWriter.toString();
 
         Element emailElement = dump.element("email");
         if (emailElement != null) {
             Map params = new HashMap();
             // put addresses
-            for (Iterator iter = emailElement.elementIterator(); iter.hasNext(); ) {
+            for (Iterator iter = emailElement.elementIterator(); iter.hasNext();) {
                 Element element = (Element) iter.next();
                 params.put(element.getName().toUpperCase(), element.getTextTrim());
             }
             params.put(EmailSender.KEY_SUBJECT, "anketa");
-            params.put(EmailSender.KEY_BODY, sWriter.toString());
-            if ( !EmailSender.sendEmail(params) )
-                log.warn("Failed to send form data:\n"+sWriter.toString());
+            params.put(EmailSender.KEY_BODY, xmlString);
+            if (!EmailSender.sendEmail(params))
+                log.warn("Failed to send form data:\n" + xmlString);
         }
 
         Element element = dump.element("dir");
         if (element != null) {
-            if ( element==null || element.getTextTrim().length()==0 )
+            if (element == null || element.getTextTrim().length() == 0)
                 throw new InvalidDataException("No dir in survey!");
             String dir = element.getTextTrim();
 
             element = dump.element("prefix");
-            if ( element==null || element.getTextTrim().length()==0 )
+            if (element == null || element.getTextTrim().length() == 0)
                 throw new InvalidDataException("No prefix in survey!");
             String prefix = element.getTextTrim();
 
             String suffix = null;
             element = dump.element("suffix");
-            if ( element != null )
+            if (element != null)
                 suffix = element.getTextTrim();
 
-            File directory = new File(FMUtils.getTemplatesDir(), "web/ankety/"+dir);
-            if ( ! directory.exists() )
+            File directory = new File(FMUtils.getTemplatesDir(), "web/ankety/" + dir);
+            if (!directory.exists())
                 directory.mkdirs();
 
             File file = File.createTempFile(prefix, suffix, directory);
             Writer writer = new BufferedWriter(new FileWriter(file));
-            writer.write(sWriter.toString());
+            writer.write(xmlString);
             writer.close();
         }
     }
