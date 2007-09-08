ddb5ce87553afe0887777a23028ae46b3076b612
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2007-09-08 16:57:21 +0000

    fixed bug #772 - povolit znacku Q
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@2100 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/utils/parser/safehtml/SafeHTMLGuard.java
+++ src/cz/abclinuxu/utils/parser/safehtml/SafeHTMLGuard.java
@@ -30,30 +30,31 @@ import java.util.*;
 public class SafeHTMLGuard {
     static final Map TAGS = new HashMap();
     static {
-        TAGS.put("B", new CheckedTag("B", true, null));
-        TAGS.put("I", new CheckedTag("I", true, null));
-        TAGS.put("P", new CheckedTag("P", false, new String[]{"ID", "CLASS"}));
-        TAGS.put("BR", new CheckedTag("BR", false, null));
         TAGS.put("A", new CheckedTag("A", true, new String[]{"HREF", "TITLE", "CLASS", "NAME", "TARGET"}));
-        TAGS.put("PRE", new CheckedTag("PRE", true, new String[]{"WIDTH", "ID", "CLASS"}));
-        TAGS.put("LI", new CheckedTag("LI", false, null));
-        TAGS.put("UL", new CheckedTag("UL", true,  new String[]{"ID"}));
-        TAGS.put("OL", new CheckedTag("OL", true, new String[]{"ID"}));
+        TAGS.put("B", new CheckedTag("B", true, null));
+        TAGS.put("BLOCKQUOTE", new CheckedTag("BLOCKQUOTE", true, new String[]{"ID"}));
+        TAGS.put("BR", new CheckedTag("BR", false, null));
+        TAGS.put("CITE", new CheckedTag("CITE", true, null));
         TAGS.put("CODE", new CheckedTag("CODE", true, null));
+        TAGS.put("DEL", new CheckedTag("DEL", true, null));
         TAGS.put("DIV", new CheckedTag("DIV", true, new String[]{"ID", "CLASS"}));
+        TAGS.put("EM", new CheckedTag("EM", true, null));
         TAGS.put("H1", new CheckedTag("H1", true, new String[]{"ID", "CLASS"}));
         TAGS.put("H2", new CheckedTag("H2", true, new String[]{"ID", "CLASS"}));
         TAGS.put("H3", new CheckedTag("H3", true, new String[]{"ID", "CLASS"}));
-        TAGS.put("EM", new CheckedTag("EM", true, null));
-        TAGS.put("STRONG", new CheckedTag("STRONG", true, null));
-        TAGS.put("CITE", new CheckedTag("CITE", true, null));
-        TAGS.put("BLOCKQUOTE", new CheckedTag("BLOCKQUOTE", true, new String[]{"ID"}));
-        TAGS.put("VAR", new CheckedTag("VAR", true, null));
         TAGS.put("HR", new CheckedTag("HR", false, null));
+        TAGS.put("I", new CheckedTag("I", true, null));
+        TAGS.put("INS", new CheckedTag("INS", true, null));
+        TAGS.put("LI", new CheckedTag("LI", false, null));
+        TAGS.put("OL", new CheckedTag("OL", true, new String[]{"ID"}));
+        TAGS.put("P", new CheckedTag("P", false, new String[]{"ID", "CLASS"}));
+        TAGS.put("PRE", new CheckedTag("PRE", true, new String[]{"WIDTH", "ID", "CLASS"}));
+        TAGS.put("Q", new CheckedTag("Q", true, new String[]{"ID"}));
+        TAGS.put("STRONG", new CheckedTag("STRONG", true, null));
         TAGS.put("TT", new CheckedTag("TT", true, null));
         TAGS.put("U", new CheckedTag("U", true, null));
-        TAGS.put("DEL", new CheckedTag("DEL", true, null));
-        TAGS.put("INS", new CheckedTag("INS", true, null));
+        TAGS.put("UL", new CheckedTag("UL", true, new String[]{"ID"}));
+        TAGS.put("VAR", new CheckedTag("VAR", true, null));
     }
 
     /**
