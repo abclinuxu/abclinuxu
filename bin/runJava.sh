47f6725dd5913ec43a0088cfe8f7a021d13f0cd5
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2008-01-13 17:51:42 +0000

    htmlparser je vyzadovan konvertorem GenerateUrlForStories, vice RAM pro nektere akce
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@2240 be82d567-0701-4079-a524-1ea639e27e29

--- bin/runJava.sh
+++ bin/runJava.sh
@@ -4,13 +4,13 @@
 
 LIBS=@SOURCE_DIR@/libs
 BUILD=@DEPLOY_ABCLINUXU@/WEB-INF/classes
-ARGS="-Xmx100m -Dabc.config=@DEPLOY_ABCLINUXU@/WEB-INF/conf/systemPrefs.xml"
+ARGS="-Xmx400m -Dabc.config=@DEPLOY_ABCLINUXU@/WEB-INF/conf/systemPrefs.xml"
 
 JARS=$LIBS/dom4j.jar:$LIBS/jaxen.jar:$LIBS/log4j.jar:$LIBS/mysql-connector.jar:$LIBS/proxool.jar
 JARS=$JARS:$LIBS/lucene.jar:$LIBS/regexp.jar:$LIBS/servlet.jar:$LIBS/whirlycache.jar:$LIBS/concurrent.jar
 JARS=$JARS:$LIBS/activation.jar:$LIBS/mail.jar:$LIBS/commons-collections.jar:$LIBS/commons-logging.jar
-JARS=$JARS:$LIBS/commons-httpclient.jar:$LIBS/commons-codec.jar:$LIBS/commons-logging.jar:$LIBS/htmlcleaner.jar
-JARS=$JARS:$LIBS/freemarker.jar
+JARS=$JARS:$LIBS/commons-httpclient.jar:$LIBS/commons-codec.jar:$LIBS/commons-logging.jar
+JARS=$JARS:$LIBS/freemarker.jar:$LIBS/htmlcleaner.jar:$LIBS/htmlparser.jar
 export CLASSPATH=$CLASSPATH:$BUILD:$JARS
 
 java $ARGS -cp $CLASSPATH $1 $2 $3 $4 $5
