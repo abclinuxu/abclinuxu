e8e31049147e84b3a710ae578dbd6d7fdd84222f
literakl <literakl@be82d567-0701-4079-a524-1ea639e27e29>
2009-02-08 21:47:24 +0000

    zmena podminky, pribyl tam novy atribut
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@3066 be82d567-0701-4079-a524-1ea639e27e29

--- bin/regular/mothly.sh
+++ bin/regular/mothly.sh
@@ -1,1 +1,1 @@
-mysql abc -e "select cislo from uzivatel where data like '%<email valid=\"yes\">%<newsletter>yes%'"
+mysql abc -e "select cislo from uzivatel where data like '%<email valid=\"yes\"%<newsletter>yes%'"
