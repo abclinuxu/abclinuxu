b7c41cb7c09a74cac17cb60370e4a1986e27b262
kapy <kapy@be82d567-0701-4079-a524-1ea639e27e29>
2009-03-17 20:40:29 +0000

    BeanFetcher allows Author to be fetched from Object[]
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/branches/devel-redakce@3106 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/utils/BeanFetcher.java
+++ src/cz/abclinuxu/utils/BeanFetcher.java
@@ -1,9 +1,15 @@
 package cz.abclinuxu.utils;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.dom4j.Element;
 import org.dom4j.Node;
 
 import cz.abclinuxu.data.Item;
+import cz.abclinuxu.data.XMLHandler;
 import cz.abclinuxu.data.view.Author;
 
 /**
@@ -13,7 +19,8 @@ import cz.abclinuxu.data.view.Author;
  * 
  */
 public class BeanFetcher {
-
+	private static final Logger log = Logger.getLogger(BeanFetcher.class);
+	
 	/**
 	 * Determines how data are fetched into bean
 	 * 
@@ -33,41 +40,25 @@ public class BeanFetcher {
 	}
 
 	/**
-	public static class ParamMapper {
-		private List<ParamMapperElement> elements;
-	
-		public ParamMapper(String...parts) {
-			if(parts.length%3!=0) 
-				throw new IllegalArgumentException("Param mapper is not following required syntax, wrong number of arguments!");
-			
-			elements = new ArrayList<ParamMapperElement>();
-			ParamMapperElement pme = null;
-			for(int i=0; i< parts.length; i++) {
-				switch(i%3) {
-				case 0:
-					pme = new ParamMapperElement();
-					pme.paramName = parts[i];
-					break;
-				case 1:
-					pme.xpath = parts[i];
-					break;
-				case 2:
-					pme.errorMessage = parts[i];
-					elements.add(pme);
-					break;
-				}
-			}			
-		} 
-	}
-	
-	
-	private static class ParamMapperElement {
-		String paramName;
-		String xpath;
-		String errorMessage;
-	}
-	*/
-	
+	 * public static class ParamMapper { private List<ParamMapperElement>
+	 * elements;
+	 * 
+	 * public ParamMapper(String...parts) { if(parts.length%3!=0) throw new
+	 * IllegalArgumentException(
+	 * "Param mapper is not following required syntax, wrong number of arguments!"
+	 * );
+	 * 
+	 * elements = new ArrayList<ParamMapperElement>(); ParamMapperElement pme =
+	 * null; for(int i=0; i< parts.length; i++) { switch(i%3) { case 0: pme =
+	 * new ParamMapperElement(); pme.paramName = parts[i]; break; case 1:
+	 * pme.xpath = parts[i]; break; case 2: pme.errorMessage = parts[i];
+	 * elements.add(pme); break; } } } }
+	 * 
+	 * 
+	 * private static class ParamMapperElement { String paramName; String xpath;
+	 * String errorMessage; }
+	 */
+
 	// Disable instantiation
 	private BeanFetcher() {
 	}
@@ -100,23 +91,96 @@ public class BeanFetcher {
 		case OMIT_XML:
 			return author;
 		case EAGER:
-			Element r = item.getData().getRootElement();
-			author.setAbout(safeRetrieveElementText(r, "/data/about"));
-			author.setAddress(safeRetrieveElementText(r, "/data/address"));
-			author.setAccountNumber(safeRetrieveElementText(r,
-					"/data/accountNumber"));
-			author.setBirthNumber(safeRetrieveElementText(r,
-					"/data/birthNumber"));
-			author.setEmail(safeRetrieveElementText(r, "/data/email"));
-			author.setLogin(safeRetrieveElementText(r, "/data/login"));
-			author.setNickname(safeRetrieveElementText(r, "/data/nickname"));
-			author.setPhone(safeRetrieveElementText(r, "/data/phone"));
-			author.setPhotoUrl(safeRetrieveElementText(r, "/data/photourl"));
-			return author;
+			Element root = item.getData().getRootElement();
+			return fillXMLProperties(author, root);
 		default:
 			throw new UnsupportedOperationException(
 					"Unable to fetch data with FetchType " + ft.toString());
 		}
+	}
+	
+	public static List<Author> fetchAuthorsFromItems(List<Item> list,
+			FetchType ft) {
+		List<Author> authors = new ArrayList<Author>(list.size());
+		for (Item i : list) {
+			authors.add(fetchAuthorFromItem(i, ft));
+		}
+		return authors;
+	}
+
+	/**
+	 * Creates author from array of objects. Expects object at specified positions
+	 * <ol>
+	 * <li>cislo</li>
+	 * <li>numeric1</li>
+	 * <li>numeric2</li>
+	 * <li>string1</li>
+	 * <li>string2</li>
+	 * <li>data</li>
+	 * <li>articleCount</li>
+	 * </ol>
+	 * @param objects Array of objects
+	 * @param ft Fetch type
+	 * @return Created author object
+	 */
+	public static Author fetchAuthorFromObjects(Object[] objects, FetchType ft) {
+		if(objects==null || objects.length!=8)
+			return null;
+		
+		Author author = new Author();
+		try {
+			author.setId((Integer)objects[0]);
+			author.setUid((Integer) objects[1]);
+			boolean active = (objects[2]!= null && objects[2].equals(1)) ? true : false;
+			author.setActive(active);
+			author.setName((String) objects[3]);
+			author.setSurname((String) objects[4]);
+			author.setArticleCount(((Long) objects[6]).intValue());
+			
+			java.sql.Timestamp date = (java.sql.Timestamp) objects[7];
+			if(date!=null) author.setLastArticleDate(new Date(date.getTime()));
+			
+			switch (ft) {
+			case OMIT_XML:
+				return author;
+			case EAGER:
+				XMLHandler handler = new XMLHandler((String) objects[5]);
+				Element root = handler.getData().getRootElement();
+				return fillXMLProperties(author, root);
+			default:
+				throw new UnsupportedOperationException(
+						"Unable to fetch data with FetchType " + ft.toString());
+			}
+			
+		}
+		catch (ClassCastException cce) {
+			log.warn("Unable to create author from array of objects",cce);
+		}
+		
+		return null;
+	}
+	
+	public static List<Author> fetchAuthorsFromObjects(List<Object[]> list, FetchType ft) {
+		List<Author> authors = new ArrayList<Author>(list.size());
+		for(Object[] objects:list) {
+			authors.add(fetchAuthorFromObjects(objects, ft));
+		}
+		return authors;
+	}
+
+	private static Author fillXMLProperties(Author author, Element root) {
+		author.setAbout(safeRetrieveElementText(root, "/data/about"));
+		author.setAddress(safeRetrieveElementText(root, "/data/address"));
+		author.setAccountNumber(safeRetrieveElementText(root,
+				"/data/accountNumber"));
+		author.setBirthNumber(safeRetrieveElementText(root,
+				"/data/birthNumber"));
+		author.setEmail(safeRetrieveElementText(root, "/data/email"));
+		author.setLogin(safeRetrieveElementText(root, "/data/login"));
+		author.setNickname(safeRetrieveElementText(root, "/data/nickname"));
+		author.setPhone(safeRetrieveElementText(root, "/data/phone"));
+		author.setPhotoUrl(safeRetrieveElementText(root, "/data/photourl"));
+		return author;
 	}
 	
 	private static String safeRetrieveElementText(Element root, String xpath) {
