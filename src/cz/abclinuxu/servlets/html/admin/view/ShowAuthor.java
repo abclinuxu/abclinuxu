0357b11502a4268f783c9f9e38aefcc8adec16d6
kapy <kapy@be82d567-0701-4079-a524-1ea639e27e29>
2009-04-26 13:37:36 +0000

    Author listing Paging fixed
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/branches/devel-redakce@3151 be82d567-0701-4079-a524-1ea639e27e29

--- src/cz/abclinuxu/servlets/html/admin/view/ShowAuthor.java
+++ src/cz/abclinuxu/servlets/html/admin/view/ShowAuthor.java
@@ -35,131 +35,130 @@ import cz.abclinuxu.utils.paging.Paging;
 
 public class ShowAuthor implements AbcAction {
 
-	/** list of found relations, that match the conditions*/
-	public static final String VAR_FOUND = "FOUND";
-	/** distinct author to be shown*/	
-	public static final String VAR_AUTHOR = "AUTHOR";
-	
-	/** form filtering */
-	public static final String VAR_FILTER = "FILTER";
+    /** list of found relations, that match the conditions */
+    public static final String VAR_FOUND = "FOUND";
+    /** distinct author to be shown */
+    public static final String VAR_AUTHOR = "AUTHOR";
 
-	public static final String PARAM_FROM = "from";
-	public static final String PARAM_AUTHOR_ID = "aId";
-	
-	public static final String ACTION_LIST = "list";
-	public static final String ACTION_SHOW = "show";
-	
-	public String process(HttpServletRequest request,
-			HttpServletResponse response, Map env) throws Exception {
+    /** form filtering */
+    public static final String VAR_FILTER = "FILTER";
+    /** Starting part of URL, until value of from parameter */
+    public static final String VAR_URL_BEFORE_FROM = "URL_BEFORE_FROM";
+    /** Final part of URL, after value of from parameter */
+    public static final String VAR_URL_AFTER_FROM = "URL_AFTER_FROM";
 
-		Map params = (Map) env.get(Constants.VAR_PARAMS);
-		User user = (User) env.get(Constants.VAR_USER);
-		String action = (String) params.get(PARAM_ACTION); 
+    public static final String PARAM_FROM = "from";
+    public static final String PARAM_AUTHOR_ID = "aId";
 
-		if (ServletUtils.handleMaintainance(request, env)) {
-			response.sendRedirect(response.encodeRedirectURL("/"));
-			return null;
-		}
+    public static final String ACTION_LIST = "list";
+    public static final String ACTION_SHOW = "show";
 
-		// check permissions
-		if (user == null)
-			return FMTemplateSelector.select("AdministrationEditorsPortal",
-					"login", env, request);
+    public String process(HttpServletRequest request, HttpServletResponse response, Map env) throws Exception {
 
-		// check permissions and create navigation
-		// tree according to permissions given
-		PwdNavigator navigator = new PwdNavigator(user,
-				PwdNavigator.NavigationType.ADMIN_AUTHORS);
+	Map params = (Map) env.get(Constants.VAR_PARAMS);
+	User user = (User) env.get(Constants.VAR_USER);
+	String action = (String) params.get(PARAM_ACTION);
 
-		if (!navigator.hasPermissions(Permissions.PERMISSION_MODIFY)) {
-			return FMTemplateSelector.select("AdministrationEditorsPortal",
-					"forbidden", env, request);
-		}
-
-		if (ACTION_SHOW.equals(action))
-			return show(request, env, navigator);
-		
-		if (ACTION_LIST.equals(action) || action == null || action.length() == 0) 
-			return list(request, env, navigator);
-
-		throw new MissingArgumentException("Chybí parametr action!");
-				
-		
+	if (ServletUtils.handleMaintainance(request, env)) {
+	    response.sendRedirect(response.encodeRedirectURL("/"));
+	    return null;
 	}
-	
-	private String list(HttpServletRequest request, Map env, PwdNavigator navigator) {
 
-		Map params = (Map) env.get(Constants.VAR_PARAMS);
-		int from = Misc.parseInt((String) params.get(PARAM_FROM),0);
-		int count = Misc.getPageSize(50, 50, env, null);
+	// check permissions
+	if (user == null)
+	    return FMTemplateSelector.select("AdministrationEditorsPortal", "login", env, request);
 
-		// store navigation structure
-		List<Link> parents = navigator.navigate();
-		env.put(Constants.VAR_PARENTS, parents);
-		
-		// create filters
-		FormFilter filter = new FormFilter(params, 
-				FormFilter.AUTHORS_BY_NAME, FormFilter.AUTHORS_BY_SURNAME,
-				FormFilter.AUTHORS_BY_CONTRACT, FormFilter.AUTHORS_BY_ACTIVE,
-				FormFilter.AUTHORS_BY_ARTICLES, FormFilter.AUTHORS_BY_RECENT);
-		
-		Paging found = null;
-		int total = 0;
-		// select according query
-		// FIXME add contract logic
-		//if(filter.containsParam(FormFilter.AUTHORS_BY_CONTRACT)) {
-			
-		//}
-		// no additional queries required
-		//else {
-			SQLTool sqlTool = SQLTool.getInstance();
-			Qualifier[] qualifiers = getQualifiers(filter, from, count);
-			List<Object[]> items = sqlTool.getAuthorsWithArticlesCount(qualifiers);
-			total = sqlTool.countAuthorWithArticlesCount(QualifierTool.removeOrderQualifiers(qualifiers));
-			found = new Paging(BeanFetcher.fetchAuthorsFromObjects(items, FetchType.EAGER), from, count, total, qualifiers);
-		//}			
-		env.put(VAR_FILTER, filter);
-		env.put(VAR_FOUND, found);
-		
-		return FMTemplateSelector.select("AdministrationShowAuthor", "list", env, request);
-	}
-	
-	private String show(HttpServletRequest request, Map env, PwdNavigator navigator) {
-		Persistence persistence = PersistenceFactory.getPersistence();
-		SQLTool sqlTool = SQLTool.getInstance();
-		Map params = (Map) env.get(Constants.VAR_PARAMS);
-		
-		// find author
-		Integer aId = null;
-		try {
-			aId = Misc.parsePossiblyWrongInt((String)params.get(PARAM_AUTHOR_ID));
-		}
-		catch (InvalidInputException iie) {
-			throw new MissingArgumentException("Chybí parametr aId!");
-		}		
+	// check permissions and create navigation
+	// tree according to permissions given
+	PwdNavigator navigator = new PwdNavigator(user, PwdNavigator.NavigationType.ADMIN_AUTHORS);
 
-		Qualifier[] qualifiers = {new CompareCondition(Field.ID,Operation.EQUAL, aId)};
-		List<Object[]> authorObjects = sqlTool.getAuthorsWithArticlesCount(qualifiers);
-		
-		if(authorObjects.isEmpty()) {
-			throw new InvalidDataException("Nepodařilo se najít rodičovskou relaci pro autora" + aId +"!");
-		}
-		
-		Author author = BeanFetcher.fetchAuthorFromObjects(authorObjects.get(0), FetchType.EAGER);
-		 
-		Link tail = new Link(author.getTitle(), "show?aId="+ author.getId()+"&amp;action=show", "Zobrazení autora");
-		env.put(Constants.VAR_PARENTS, navigator.navigate(tail));		
-		env.put(VAR_AUTHOR, author);
-		return FMTemplateSelector.select("AdministrationShowAuthor", "show", env, request);
+	if (!navigator.hasPermissions(Permissions.PERMISSION_MODIFY)) {
+	    return FMTemplateSelector.select("AdministrationEditorsPortal", "forbidden", env, request);
 	}
+
+	if (ACTION_SHOW.equals(action))
+	    return show(request, env, navigator);
+
+	if (ACTION_LIST.equals(action) || action == null || action.length() == 0)
+	    return list(request, env, navigator);
+
+	throw new MissingArgumentException("Chybí parametr action!");
+
+    }
+
+    private String list(HttpServletRequest request, Map env, PwdNavigator navigator) {
+
+	Map params = (Map) env.get(Constants.VAR_PARAMS);
+	int from = Misc.parseInt((String) params.get(PARAM_FROM), 0);
+	int count = Misc.getPageSize(50, 50, env, null);
+
+	// store navigation structure
+	List<Link> parents = navigator.navigate();
+	env.put(Constants.VAR_PARENTS, parents);
+
+	// create filters
+	FormFilter filter = new FormFilter(params, FormFilter.AUTHORS_BY_NAME, FormFilter.AUTHORS_BY_SURNAME, FormFilter.AUTHORS_BY_CONTRACT, FormFilter.AUTHORS_BY_ACTIVE, FormFilter.AUTHORS_BY_ARTICLES, FormFilter.AUTHORS_BY_RECENT);
+
+	Paging found = null;
+	int total = 0;
+	// select according query
+	// FIXME add contract logic
+	// if(filter.containsParam(FormFilter.AUTHORS_BY_CONTRACT)) {
+
+	// }
+	// no additional queries required
+	// else {
+	SQLTool sqlTool = SQLTool.getInstance();
+	Qualifier[] qualifiers = getQualifiers(filter, from, count);
+	List<Object[]> items = sqlTool.getAuthorsWithArticlesCount(qualifiers);
+	total = sqlTool.countAuthorWithArticlesCount(QualifierTool.removeOrderQualifiers(qualifiers));
+	found = new Paging(BeanFetcher.fetchAuthorsFromObjects(items, FetchType.EAGER), from, count, total, qualifiers);
+	// }
+	env.put(VAR_FILTER, filter);
+	env.put(VAR_FOUND, found);
 	
-	private Qualifier[] getQualifiers(FormFilter filter, int from, int count) {
-		List<Qualifier> qualifiers = filter.getQualifiers();
-		// sort by surname in ascending order
-		qualifiers.add(Qualifier.SORT_BY_STRING2);
-		qualifiers.add(Qualifier.ORDER_ASCENDING);
-		qualifiers.add(new LimitQualifier(from, count));
-		
-		return qualifiers.toArray(Qualifier.ARRAY_TYPE);
+	// store url links
+	env.put(VAR_URL_BEFORE_FROM, "/sprava/redakce/autori/?from=");
+	env.put(VAR_URL_AFTER_FROM, filter.encodeAsURL());
+
+	return FMTemplateSelector.select("AdministrationShowAuthor", "list", env, request);
+    }
+
+    private String show(HttpServletRequest request, Map env, PwdNavigator navigator) {
+	Persistence persistence = PersistenceFactory.getPersistence();
+	SQLTool sqlTool = SQLTool.getInstance();
+	Map params = (Map) env.get(Constants.VAR_PARAMS);
+
+	// find author
+	Integer aId = null;
+	try {
+	    aId = Misc.parsePossiblyWrongInt((String) params.get(PARAM_AUTHOR_ID));
+	} catch (InvalidInputException iie) {
+	    throw new MissingArgumentException("Chybí parametr aId!");
 	}
+
+	Qualifier[] qualifiers = { new CompareCondition(Field.ID, Operation.EQUAL, aId) };
+	List<Object[]> authorObjects = sqlTool.getAuthorsWithArticlesCount(qualifiers);
+
+	if (authorObjects.isEmpty()) {
+	    throw new InvalidDataException("Nepodařilo se najít rodičovskou relaci pro autora" + aId + "!");
+	}
+
+	Author author = BeanFetcher.fetchAuthorFromObjects(authorObjects.get(0), FetchType.EAGER);
+
+	Link tail = new Link(author.getTitle(), "show?aId=" + author.getId() + "&amp;action=show", "Zobrazení autora");
+	env.put(Constants.VAR_PARENTS, navigator.navigate(tail));
+	env.put(VAR_AUTHOR, author);
+	return FMTemplateSelector.select("AdministrationShowAuthor", "show", env, request);
+    }
+
+    private Qualifier[] getQualifiers(FormFilter filter, int from, int count) {
+	List<Qualifier> qualifiers = filter.getQualifiers();
+	// sort by surname in ascending order
+	qualifiers.add(Qualifier.SORT_BY_STRING2);
+	qualifiers.add(Qualifier.ORDER_ASCENDING);
+	qualifiers.add(new LimitQualifier(from, count));
+
+	return qualifiers.toArray(Qualifier.ARRAY_TYPE);
+    }
 }
--- src/cz/abclinuxu/utils/forms/FormFilter.java
+++ src/cz/abclinuxu/utils/forms/FormFilter.java
@@ -1,5 +1,7 @@
 package cz.abclinuxu.utils.forms;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -23,269 +25,263 @@ import cz.abclinuxu.utils.Misc;
  * 
  */
 public class FormFilter {
-	private static final Logger log = Logger.getLogger(FormFilter.class);
+    private static final Logger log = Logger.getLogger(FormFilter.class);
 
-	public static final String AUTHORS_BY_NAME = "filterAuthorsByName";
-	public static final String AUTHORS_BY_SURNAME = "filterAuthorsBySurname";
-	public static final String AUTHORS_BY_CONTRACT = "filterAuthorsByContract";
-	public static final String AUTHORS_BY_ACTIVE = "filterAuthorsByActive";
-	public static final String AUTHORS_BY_ARTICLES = "filterAuthorsByArticles";
-	public static final String AUTHORS_BY_RECENT = "filterAuthorsByRecent";
+    public static final String AUTHORS_BY_NAME = "filterAuthorsByName";
+    public static final String AUTHORS_BY_SURNAME = "filterAuthorsBySurname";
+    public static final String AUTHORS_BY_CONTRACT = "filterAuthorsByContract";
+    public static final String AUTHORS_BY_ACTIVE = "filterAuthorsByActive";
+    public static final String AUTHORS_BY_ARTICLES = "filterAuthorsByArticles";
+    public static final String AUTHORS_BY_RECENT = "filterAuthorsByRecent";
 
-	/** This is string prefix for LIKE CompareConditions */
-	private static final String LIKE_PREFIX = "";
-	/** This is strig suffix for LIKE CompareConditions */
-	private static final String LIKE_SUFFIX = "%";
+    /** This is string prefix for LIKE CompareConditions */
+    private static final String LIKE_PREFIX = "";
+    /** This is strig suffix for LIKE CompareConditions */
+    private static final String LIKE_SUFFIX = "%";
 
-	private Map<String, FilterQualifier> filters;
+    private Map<String, FilterQualifier> filters;
 
-	/**
-	 * Creates form filter
-	 * 
-	 * @param params
-	 *            Parameters passed with HTTP header
-	 * @param filterNames
-	 *            Which filters are applied
-	 */
-	public FormFilter(Map<? super Object, ? super Object> params,
-			String... filterNames) {
+    /**
+     * Creates form filter
+     * 
+     * @param params Parameters passed with HTTP header
+     * @param filterNames Which filters are applied
+     */
+    public FormFilter(Map<? super Object, ? super Object> params, String... filterNames) {
 
-		this.filters = new HashMap<String, FilterQualifier>();
+	this.filters = new HashMap<String, FilterQualifier>();
 
-		for (String paramName : filterNames) {
-			// skip parameter recognition if empty
-			Object param = params.get(paramName);
-			if (param == null || param.toString().length() == 0)
-				continue;
-			initFilter(param, paramName);
-		}
+	for (String paramName : filterNames) {
+	    // skip parameter recognition if empty
+	    Object param = params.get(paramName);
+	    if (param == null || param.toString().length() == 0)
+		continue;
+	    initFilter(param, paramName);
+	}
+    }
+
+    /**
+     * Allows qualifier to be added manually after filter is created
+     * 
+     * @param paramName Name of parameter (or filter)
+     * @param qualifer Qualifier if condition matches
+     * @param formValue Value passed from form
+     * @return Modified instance of filter
+     */
+    public FormFilter appendFilterQualifier(String paramName, Qualifier qualifer, String formValue) {
+	filters.put(paramName, new FilterQualifier(qualifer, formValue));
+	return this;
+    }
+
+    /**
+     * Gets qualifiers stored in this filter
+     * 
+     * @return List of used qualifiers
+     */
+    public List<Qualifier> getQualifiers() {
+	List<Qualifier> qualifiers = new ArrayList<Qualifier>();
+	for (FilterQualifier fq : filters.values()) {
+	    if (fq.qualifier != null)
+		qualifiers.add(fq.qualifier);
+	}
+	return qualifiers;
+    }
+
+    /**
+     * Gets qualifiers stored in this filter as array
+     * 
+     * @return Array of used qualifiers
+     */
+    public Qualifier[] getQualifiersAsArray() {
+	return getQualifiers().toArray(Qualifier.ARRAY_TYPE);
+    }
+
+    /**
+     * Returns data in filter as part of url GET string. This string is encoded
+     * according to UTF-8 {@code application/x-www-form-urlencoded} MIME format.
+     * String begins with &amp; character
+     * 
+     * @return Encoded content of filter or {@code null} if error occurred
+     */
+    public String encodeAsURL() {
+	StringBuilder sb = new StringBuilder();
+
+	for (Map.Entry<String, FilterQualifier> entry : filters.entrySet()) {
+	    try {
+		sb.append("&amp;")
+		    .append(entry.getKey())
+		    .append('=')
+		    .append(URLEncoder.encode(entry.getValue().formValue, "UTF-8"));
+	    } catch (UnsupportedEncodingException uee) {
+		log.error("Couldn't not convert form parameters to UTF-8 URL encoded string in string: " + entry.getValue().formValue, uee);
+		return null;
+	    }
+	}
+	return sb.toString();
+    }
+
+    /**
+     * Checks whether for given parameter value was passed
+     * 
+     * @param param Name of parameter
+     * @param value Value of parameter
+     * @return {@code true} if matched, {@code false} otherwise
+     */
+    public boolean checked(String param, String value) {
+
+	if (log.isDebugEnabled()) {
+	    log.debug("paramName: " + param + " value: " + value);
+	    log.debug("found: " + filters.get(param));
 	}
 
-	/**
-	 * Allows qualifier to be added manually after filter is created
-	 * 
-	 * @param paramName
-	 *            Name of parameter (or filter)
-	 * @param qualifer
-	 *            Qualifier if condition matches
-	 * @param formValue
-	 *            Value passed from form
-	 * @return Modified instance of filter
-	 */
-	public FormFilter appendFilterQualifier(String paramName,
-			Qualifier qualifer, String formValue) {
-		filters.put(paramName, new FilterQualifier(qualifer, formValue));
-		return this;
+	FilterQualifier fq = filters.get(param);
+	return fq != null && value.equals(fq.formValue);
+    }
+
+    /**
+     * Returns value of parameter, as passed from form
+     * 
+     * @param param Value passed from form
+     * @return Value of parameter passed od {@code null}
+     */
+    public String value(String param) {
+	FilterQualifier fq = filters.get(param);
+	return (fq != null) ? fq.formValue : "";
+    }
+
+    /**
+     * Adds filter for given name and given object
+     * 
+     * @param param Object found for given filter
+     * @param filterName Name of filter
+     */
+    private void initFilter(Object param, String filterName) {
+	// TODO Do we have to sanify somehow this input?
+	String formValue = param.toString();
+
+	// select according to type of condition
+
+	// author name has append to be regexp
+	if (AUTHORS_BY_NAME.equals(filterName)) {
+	    filters.put(filterName, new FilterQualifier(new CompareCondition(Field.STRING1, Operation.LIKE, LIKE_PREFIX + param
+		+ LIKE_SUFFIX), formValue));
+	} else if (AUTHORS_BY_SURNAME.equals(filterName)) {
+	    filters.put(filterName, new FilterQualifier(new CompareCondition(Field.STRING2, Operation.LIKE, LIKE_PREFIX + param
+		+ LIKE_SUFFIX), formValue));
+	} else if (AUTHORS_BY_ACTIVE.equals(filterName)) {
+	    int value = Misc.parseInt(formValue, 0);
+	    filters.put(filterName, new FilterQualifier(construct(Field.NUMERIC2, value, Operation.EQUAL, 1, Operation.EQUAL, IsNullCheck.BEFORE_CAP), formValue));
+	} else if (AUTHORS_BY_ARTICLES.equals(filterName)) {
+	    int value = Misc.parsePossiblyWrongInt(formValue);
+	    filters.put(filterName, new FilterQualifier(construct(Field.COUNTER, value, Operation.SMALLER, 101, Operation.GREATER_OR_EQUAL, IsNullCheck.BEFORE_CAP), formValue));
+	} else if (AUTHORS_BY_RECENT.equals(filterName)) {
+	    Calendar cal = Calendar.getInstance();
+	    Calendar ref = (Calendar) cal.clone();
+	    ref.set(Calendar.MONTH, ref.get(Calendar.MONTH) - 24);
+	    java.sql.Date cap = new java.sql.Date(ref.getTimeInMillis());
+
+	    int intValue = Misc.parsePossiblyWrongInt(formValue);
+	    java.sql.Date value = null;
+	    if (intValue == 25) {
+		ref.set(Calendar.MILLISECOND, ref.get(Calendar.MILLISECOND) - 1);
+		value = new java.sql.Date(ref.getTimeInMillis());
+	    } else {
+		cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) - intValue);
+		value = new java.sql.Date(cal.getTimeInMillis());
+	    }
+
+	    filters.put(filterName, new FilterQualifier(construct(Field.WHEN, value, Operation.SMALLER_OR_EQUAL, cap, Operation.GREATER, IsNullCheck.NO_CHECK), formValue));
+	} else if (AUTHORS_BY_CONTRACT.equals(filterName)) {
+	    filters.put(filterName, new FilterQualifier(null, formValue));
 	}
 
-	/**
-	 * Gets qualifiers stored in this filter
-	 * 
-	 * @return List of used qualifiers
-	 */
-	public List<Qualifier> getQualifiers() {
-		List<Qualifier> qualifiers = new ArrayList<Qualifier>();
-		for (FilterQualifier fq : filters.values()) {
-			if (fq.qualifier != null)
-				qualifiers.add(fq.qualifier);
-		}
-		return qualifiers;
+    }
+
+    /**
+     * Constructs Compare condition or nested condition with IS NULL variant
+     * 
+     * @param <T> Type of cap and value
+     * @param field Field to be compared
+     * @param value Value passed from form
+     * @param before Operation applied to comparison before cap (excluded) is
+     *            reached
+     * @param cap Value of cap
+     * @param after Operation applied to comparison after cap (included) is
+     *            reached
+     * @param inc How to determine IS NULL presence
+     * @return Qualifier for given field
+     */
+    private <T extends Comparable<? super T>> Qualifier construct(Field field, T value, Operation before,
+	T cap, Operation after, IsNullCheck inc) {
+
+	boolean beforeCap = cap.compareTo(value) > 0;
+	Operation op = (beforeCap) ? before : after;
+
+	List<Qualifier> list = new ArrayList<Qualifier>();
+
+	switch (inc) {
+	case BEFORE_CAP:
+	    if (beforeCap)
+		list.add(new CompareCondition(field, Operation.IS_NULL, 0));
+	    list.add(new CompareCondition(field, op, value));
+	    break;
+	case AFTER_CAP:
+	    if (!beforeCap)
+		list.add(new CompareCondition(field, Operation.IS_NULL, 0));
+	    list.add(new CompareCondition(field, op, value));
+	    break;
+	case ALL_CHECK:
+	    list.add(new CompareCondition(field, Operation.IS_NULL, 0));
+	case NO_CHECK:
+	    list.add(new CompareCondition(field, op, value));
+	    break;
 	}
 
-	/**
-	 * Gets qualifiers stored in this filter as array
-	 * 
-	 * @return Array of used qualifiers
-	 */
-	public Qualifier[] getQualifiersAsArray() {
-		return getQualifiers().toArray(Qualifier.ARRAY_TYPE);
+	if (list.size() == 1)
+	    return list.get(0);
+
+	return new NestedCondition(list, LogicalOperation.OR);
+
+    }
+
+    /**
+     * Filter qualifier contains value passed from form and qualifier for
+     * narrowing SQL query. This qualifier can be empty, if some different
+     * approach to narrow query is needed.
+     * 
+     * @author kapy
+     * 
+     */
+    private static class FilterQualifier {
+	Qualifier qualifier;
+	String formValue;
+
+	public FilterQualifier(Qualifier qualifer, String formValue) {
+	    this.qualifier = qualifer;
+	    this.formValue = formValue;
 	}
 
-	/**
-	 * Checks whether for given parameter value was passed
-	 * 
-	 * @param param
-	 *            Name of parameter
-	 * @param value
-	 *            Value of parameter
-	 * @return {@code true} if matched, {@code false} otherwise
-	 */
-	public boolean checked(String param, String value) {
-
-		if (log.isDebugEnabled()) {
-			log.debug("paramName: " + param + " value: " + value);
-			log.debug("found: " + filters.get(param));
-		}
-
-		FilterQualifier fq = filters.get(param);
-		return fq != null && value.equals(fq.formValue);
+	@Override
+	public String toString() {
+	    return "FQ:" + formValue;
 	}
+    }
 
-	/**
-	 * Returns value of parameter, as passed from form
-	 * 
-	 * @param param
-	 *            Value passed from form
-	 * @return Value of parameter passed od {@code null}
-	 */
-	public String value(String param) {
-		FilterQualifier fq = filters.get(param);
-		return (fq != null) ? fq.formValue : "";
-	}
-
-	/**
-	 * Adds filter for given name and given object
-	 * 
-	 * @param param
-	 *            Object found for given filter
-	 * @param filterName
-	 *            Name of filter
-	 */
-	private void initFilter(Object param, String filterName) {
-		// TODO Do we have to sanify somehow this input?
-		String formValue = param.toString();
-
-		// select according to type of condition
-
-		// author name has append to be regexp
-		if (AUTHORS_BY_NAME.equals(filterName)) {
-			filters.put(filterName, new FilterQualifier(new CompareCondition(
-					Field.STRING1, Operation.LIKE, LIKE_PREFIX + param
-							+ LIKE_SUFFIX), formValue));
-		} else if (AUTHORS_BY_SURNAME.equals(filterName)) {
-			filters.put(filterName, new FilterQualifier(new CompareCondition(
-					Field.STRING2, Operation.LIKE, LIKE_PREFIX + param
-							+ LIKE_SUFFIX), formValue));
-		} else if (AUTHORS_BY_ACTIVE.equals(filterName)) {
-			int value = Misc.parseInt(formValue, 0);
-			filters.put(filterName, new FilterQualifier(construct(
-					Field.NUMERIC2, value, Operation.EQUAL, 1, Operation.EQUAL,
-					IsNullCheck.BEFORE_CAP), formValue));
-		} else if (AUTHORS_BY_ARTICLES.equals(filterName)) {
-			int value = Misc.parsePossiblyWrongInt(formValue);
-			filters.put(filterName, new FilterQualifier(construct(
-					Field.COUNTER, value, Operation.SMALLER, 101,
-					Operation.GREATER_OR_EQUAL, IsNullCheck.BEFORE_CAP),
-					formValue));
-		} else if (AUTHORS_BY_RECENT.equals(filterName)) {
-			Calendar cal = Calendar.getInstance();
-			Calendar ref = (Calendar) cal.clone();
-			ref.set(Calendar.MONTH, ref.get(Calendar.MONTH) - 24);
-			java.sql.Date cap = new java.sql.Date(ref.getTimeInMillis());
-
-			int intValue = Misc.parsePossiblyWrongInt(formValue);
-			java.sql.Date value = null;
-			if (intValue == 25) {
-				ref
-						.set(Calendar.MILLISECOND, ref
-								.get(Calendar.MILLISECOND) - 1);
-				value = new java.sql.Date(ref.getTimeInMillis());
-			} else {
-				cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) - intValue);
-				value = new java.sql.Date(cal.getTimeInMillis());
-			}
-
-			filters.put(filterName, new FilterQualifier(construct(Field.WHEN,
-					value, Operation.SMALLER_OR_EQUAL, cap, Operation.GREATER,
-					IsNullCheck.NO_CHECK), formValue));
-		} else if (AUTHORS_BY_CONTRACT.equals(filterName)) {
-			filters.put(filterName, new FilterQualifier(null, formValue));
-		}
-
-	}
-
-	/**
-	 * Constructs Compare condition or nested condition with IS NULL variant
-	 * 
-	 * @param <T>
-	 *            Type of cap and value
-	 * @param field
-	 *            Field to be compared
-	 * @param value
-	 *            Value passed from form
-	 * @param before
-	 *            Operation applied to comparison before cap (excluded) is
-	 *            reached
-	 * @param cap
-	 *            Value of cap
-	 * @param after
-	 *            Operation applied to comparison after cap (included) is
-	 *            reached
-	 * @param inc
-	 *            How to determine IS NULL presence
-	 * @return Qualifier for given field
-	 */
-	private <T extends Comparable<? super T>> Qualifier construct(Field field,
-			T value, Operation before, T cap, Operation after, IsNullCheck inc) {
-
-		boolean beforeCap = cap.compareTo(value) > 0;
-		Operation op = (beforeCap) ? before : after;
-
-		List<Qualifier> list = new ArrayList<Qualifier>();
-
-		switch (inc) {
-		case BEFORE_CAP:
-			if (beforeCap)
-				list.add(new CompareCondition(field, Operation.IS_NULL, 0));
-			list.add(new CompareCondition(field, op, value));
-			break;
-		case AFTER_CAP:
-			if (!beforeCap)
-				list.add(new CompareCondition(field, Operation.IS_NULL, 0));
-			list.add(new CompareCondition(field, op, value));
-			break;
-		case ALL_CHECK:
-			list.add(new CompareCondition(field, Operation.IS_NULL, 0));
-		case NO_CHECK:
-			list.add(new CompareCondition(field, op, value));
-			break;
-		}
-
-		if (list.size() == 1)
-			return list.get(0);
-
-		return new NestedCondition(list, LogicalOperation.OR);
-
-	}
-
-	/**
-	 * Filter qualifier contains value passed from form and qualifier for
-	 * narrowing SQL query. This qualifier can be empty, if some different
-	 * approach to narrow query is needed.
-	 * 
-	 * @author kapy
-	 * 
-	 */
-	private static class FilterQualifier {
-		Qualifier qualifier;
-		String formValue;
-
-		public FilterQualifier(Qualifier qualifer, String formValue) {
-			this.qualifier = qualifer;
-			this.formValue = formValue;
-		}
-
-		@Override
-		public String toString() {
-			return "FQ:" + formValue;
-		}
-	}
-
-	/**
-	 * Determines whether IS NULL condition is added and when
-	 * 
-	 * @author kapy
-	 * 
-	 */
-	private enum IsNullCheck {
-		/** if value is before cap, add IS NULL condition */
-		BEFORE_CAP,
-		/** if value is after cap, add IS NULL condition */
-		AFTER_CAP,
-		/** do not add IS NULL condition */
-		NO_CHECK,
-		/** add is null condition in both cases */
-		ALL_CHECK
-	};
+    /**
+     * Determines whether IS NULL condition is added and when
+     * 
+     * @author kapy
+     * 
+     */
+    private enum IsNullCheck {
+	/** if value is before cap, add IS NULL condition */
+	BEFORE_CAP,
+	/** if value is after cap, add IS NULL condition */
+	AFTER_CAP,
+	/** do not add IS NULL condition */
+	NO_CHECK,
+	/** add is null condition in both cases */
+	ALL_CHECK
+    };
 
 }
