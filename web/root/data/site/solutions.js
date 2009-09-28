f2992327dce63620a784bae165e9763bca9e0ee6
LubosD <LubosD@be82d567-0701-4079-a524-1ea639e27e29>
2009-09-28 13:37:13 +0000

    More solutions work
    git-svn-id: https://zdrojaky.abclinuxu.cz/svn-abclinuxu/trunk@3220 be82d567-0701-4079-a524-1ea639e27e29

--- web/root/data/site/solutions.js
+++ web/root/data/site/solutions.js
@@ -4,40 +4,36 @@ jQuery(document).ready(function(){
 		
 		reverting = jQuery(this).hasClass("ds_solutionUnset");
 		
-		var votes;
 		var link = this;
 		
 		jQuery.get(this.href+"&ajax=true", function(text) {
-			votes = text;
-			
-			if (votes != "-1") {
-				reseni = jQuery(link).parent().parent().find("div.ds_reseni");
-				if (votes == "0")
-					reseni.hide();
-				else {
-					reseni.show();
-					reseni.html("Řešení: "+votes+"&times;");
-				}
-				
-				if (!reverting) {
-					link.href = jQuery(link).attr("href").replace("setSolution", "unsetSolution");
-					link.innerHTML = "Není řešením";
-					jQuery(link).removeClass("ds_solutionSet");
-					jQuery(link).addClass("ds_solutionUnset");
-				} else {
-					link.href = jQuery(link).attr("href").replace("unsetSolution", "setSolution");
-					link.innerHTML = "Označit jako řešení";
-					jQuery(link).removeClass("ds_solutionUnset");
-					jQuery(link).addClass("ds_solutionSet");
-				}
-			}
+                        reseni = jQuery(link).parent().parent().find("div.ds_reseni");
+                        if (text == "")
+                                reseni.hide();
+                        else {
+                                reseni.show();
+                                reseni.html(text);
+                        }
+
+                        if (!reverting) {
+                                link.href = jQuery(link).attr("href").replace("setSolution", "unsetSolution");
+                                link.innerHTML = "Není řešením";
+                                jQuery(link).removeClass("ds_solutionSet");
+                                jQuery(link).addClass("ds_solutionUnset");
+                        } else {
+                                link.href = jQuery(link).attr("href").replace("unsetSolution", "setSolution");
+                                link.innerHTML = "Označit jako řešení";
+                                jQuery(link).removeClass("ds_solutionUnset");
+                                jQuery(link).addClass("ds_solutionSet");
+                        }
 		});
 	});
 
 });
 
 function showCommentVoters(threadId) {
-    jQuery.get("/EditDiscussion/"+Page.relationID+"?threadId="+threadId+"&action=showVoters", function(text) {
-
+    jQuery.get("/ViewDiscussion/"+Page.relationID+"?threadId="+threadId+"&action=showVoters&fullList=yes", function(text) {
+        reseni = jQuery("#showMore-"+threadId).parent();
+        reseni.html(text);
     });
 }
